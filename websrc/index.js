724fae445e6aa28fff1f5e59b68690f1c67f49fd
pthomison <p.thomison@gmail.com>
2022-09-04 12:09:27 -0700

    cleaning

--- websrc/index.js
+++ websrc/index.js
@@ -6,44 +6,6 @@ import App from './app.vue';
 import 'bootstrap';
 import './index.scss';
 
-// const app = Vue.createApp({
-// 	data() {
-// 		return {
-// 			countstart: 0,
-// 			countdown: 0,
-// 			socket: new WebSocket("ws://127.0.0.1:5050/ws"),
-// 		}
-// 	},
-// 	methods: {
-// 		start(countstart) {
-// 			this.countstart = countstart
-// 			this.countdown = countstart
-
-// 			console.log("starting countdown at " + this.countstart)
-
-// 			this.socket.send(this.countstart)
-// 		},
-// 	},
-// 	computed: {
-// 		percentage() {
-// 			return (((this.countstart - this.countdown)/this.countstart) * 100)
-// 		}
-// 	},
-// 	mounted() {
-// 		var component = this
-
-// 		this.socket.onopen = function () {
-// 			console.log("Status: Connected\n");
-// 		};
-
-// 		this.socket.onmessage = function (msg) {
-// 			console.log("\nServer: " + msg.data + "\n");
-// 			component.countdown = parseInt(msg.data)
-// 		};
-
-// 	}
-// })
-
 const root = Vue.createApp({})
 
 root.component('app', App)
